799f88acea1a7e79ebe3ddd136c25ba99da3f604
theomarie <theomarie@protonmail.com>
2024-06-18 14:14:45 +0200

    Initial Commit

---
+++ fisherApp/fisherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  fisherAppApp.swift
+//  fisherApp
+//
+//  Created by Theo Marie on 18/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct fisherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
